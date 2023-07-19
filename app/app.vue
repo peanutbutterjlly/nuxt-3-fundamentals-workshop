ff4045cc8dd573983a2dba20197a29329721f258
peanutbutterjlly <steverios_sr@yahoo.com>
2023-07-19 15:41:05 -0500

    starting a project and scaffolding stuff

---
+++ app/app.vue
@@ -0,0 +1,30 @@
+<script>
+import { defineNuxtComponent } from 'nuxt/app';
+
+export default defineNuxtComponent({
+  data: () => ({
+    todoList: [],
+  }),
+  methods: {
+    fetchTodoList() {
+      fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => {
+        this.todoList = json;
+      });
+    }
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1>Hello world</h1>
+    <button @click="fetchTodoList()">Fetch</button>
+    <ul>
+      <li v-for="todo in todoList" :key="`todo-${todo.id}`">
+        <input type="checkbox" :checked="todo.completed" /> {{ todo.title }}
+      </li>
+    </ul>
+  </div>
+</template>
