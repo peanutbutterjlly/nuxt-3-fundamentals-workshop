40655362979dba9a5f782f73aa8de3cbf17dac85
steve <steverios_sr@yahoo.com>
2024-05-13 19:11:00 -0500

    fleshed out code for ex 1

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,37 @@
-<script></script>
+<script>
+import { defineNuxtComponent } from '#app';
+
+export default defineNuxtComponent({
+  data: () => ({
+    photoList: [],
+  }),
+  methods: {
+    fetchPhotoList() {
+      fetch('https://jsonplaceholder.typicode.com/photos/')
+        .then((res) => res.json())
+        .then((json) => {
+          this.photoList = json;
+        });
+    },
+  },
+});
+</script>
 
 <template>
-  <h1>Photo Gallery</h1>
+  <div>
+    <h1>Photo Gallery</h1>
+    <button @click="fetchPhotoList">Get Photos</button>
+    <!-- <pre>{{ photoList }}</pre> -->
+    <ul>
+      <li v-for="photo in photoList" :key="`photo-id-${photo.id}`">
+        <img :src="photo.thumbnailUrl" alt="" />
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style></style>
+<style>
+ul {
+  list-style-type: none;
+}
+</style>
